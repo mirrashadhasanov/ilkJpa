e58e293ab87b93b0bf5bd633ca45a97ad5e2eb8e
Mirrashad Hasanov <mirrashadhasanov@mail.ru>
2020-11-10 19:16:18 +0400

    Initial commit

---
+++ src/main/java/com/mirrashadhasanov/blog/Customer.java
@@ -0,0 +1,24 @@
+package com.mirrashadhasanov.blog;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private int age;
+    private String name;
+
+    Customer(){}
+
+    public Customer (String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+}
---
+++ src/main/java/com/mirrashadhasanov/blog/CustomerController.java
@@ -0,0 +1,41 @@
+package com.mirrashadhasanov.blog;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+    private CustomerRepository customerRepository;
+
+    public CustomerController(CustomerRepository customerRepository){
+        this.customerRepository = customerRepository;
+    }
+
+    @GetMapping
+    List<Customer> customers(){
+        return customerRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    Optional<Customer> byId(@PathVariable Long id){
+        return customerRepository.findById(id);
+    }
+
+    @PostMapping
+    Customer save(Customer customer){
+        return customerRepository.save(customer);
+    }
+
+    @DeleteMapping("/{customer}")
+    void delete(@PathVariable Customer customer){
+        customerRepository.delete(customer);
+    }
+
+    @GetMapping("/search")
+    List<Customer> searchByAge(@RequestParam int age){
+        return customerRepository.findByAge(age);
+    }
+}
